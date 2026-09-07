361274babc198cdfc62f9651b4a966a7e1ea4e4e
Mahyar Behnamrad <MahyarBehnamrad@Gmail.com>
2026-09-07 14:00:37 +0330

    Add search filters and error handling improvements

---
+++ StudentManagementSystem/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentManagementSystem.Controllers;
+
+public class ErrorController : Controller
+{
+    [Route("Error/{statusCode}")]
+    public IActionResult HttpStatusCodeHandler(int statusCode)
+    {
+        if (statusCode == 404)
+        {
+            ViewBag.ErrorMessage = "The page or record you requested could not be found.";
+        }
+        else
+        {
+            ViewBag.ErrorMessage = "An unexpected error occurred.";
+        }
+
+        ViewBag.StatusCode = statusCode;
+
+        return View("StatusCode");
+    }
+}
--- StudentManagementSystem/Program.cs
+++ StudentManagementSystem/Program.cs
@@ -35,6 +35,8 @@ app.UseAuthorization();
 
 app.MapStaticAssets();
 
+app.UseStatusCodePagesWithReExecute("/Error/{0}");
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
